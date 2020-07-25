4e52f6d353ca190fb21aa51ce671fb62f4fa08f0
Carly <carlyne.lapierre@gmail.com>
2020-07-25 22:40:39 +0200

    ajout du style et de la librairie de transformation text -> html

--- server.js
+++ server.js
@@ -2,17 +2,24 @@ import 'dotenv/config';
 import express from "express";
 import cors from 'cors';
 
+const port = process.env.PORT || 8000;
 const app = express();
 
-// Autoriser l'accèsnavigateur aux users
+// Autoriser l'accès navigateur aux users
 app.use(cors());
+
+// Fichier public pour les assets
+app.use(express.static(__dirname + '/public'));
+
+// définir le moteur de modèle sur ejs
+app.set('view engine', 'ejs');
 
 // Création de la route
 app.get('/', (request, response) => {
-    response.send("Hello App !")
+    response.render('index')
 })
 
 // Port d'écoute
-app.listen(process.env.PORT, () => {
-    console.log("md-tool listening on port " + process.env.PORT);
+app.listen(port, () => {
+    console.log("md-tool listening on port http://localhost:" + port);
 })
